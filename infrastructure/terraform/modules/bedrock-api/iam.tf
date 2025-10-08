afcbc8f05ff9faecf64625ddcef000638f908d52
Chris Arsenault <carsena2@gmail.com>
2025-10-08 00:54:15 -0700

    add instance profile to allowed invocations

--- infrastructure/terraform/modules/bedrock-api/iam.tf
+++ infrastructure/terraform/modules/bedrock-api/iam.tf
@@ -46,7 +46,8 @@ data "aws_iam_policy_document" "lambda_inline" {
     ]
     # Scope down to the chosen model in this region; widen if you support multiple
     resources = [
-      "arn:aws:bedrock:${var.aws_region}::foundation-model/${var.bedrock_model_id}"
+      "arn:aws:bedrock:${var.aws_region}::foundation-model/${var.bedrock_model_id}",
+      aws_bedrock_inference_profile.model_instance_profile.arn
     ]
   }
 }
